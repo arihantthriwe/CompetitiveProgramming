3372194bf52b0bf2d963b3ff37686e887bf4460d
Arihant Jain <85555201+arihantthriwe@users.noreply.github.com>
2022-03-24 11:22:06 +0530

    Time: 53 ms (74.64%), Space: 14.6 MB (21.29%) - LeetHub

---
+++ 1-two-sum/1-two-sum.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+       var dict = [Int : Int]()
+        for (i, num) in nums.enumerated() {
+            if let index = dict[target - num]{
+                if i != index{
+                    return [index, i]
+                }  
+            }
+            dict[num] = i                    
+        }
+        return [0, 0]
+    }
+}
