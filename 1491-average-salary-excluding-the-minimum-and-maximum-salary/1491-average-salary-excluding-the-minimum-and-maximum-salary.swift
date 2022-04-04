e6a2c91c7a59ca712d977048f2fff79911be9e74
Arihant Jain <85555201+arihantthriwe@users.noreply.github.com>
2022-04-04 12:11:31 +0530

    Time: 15 ms (5.11%), Space: 14.1 MB (92.05%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.swift
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.swift
@@ -1,11 +1,5 @@
 class Solution {
     func average(_ salary: [Int]) -> Double {
-        var sum = 0
-        for s in salary{
-            sum += s
-        }
-        var st = salary
-        st.sort()
-        return (Double(sum - st[0] - st[st.count - 1])/Double(salary.count - 2))
+        Double(salary.reduce(0, +) - (salary.min()! + salary.max()!)) / Double(salary.count - 2)
     }
 }
